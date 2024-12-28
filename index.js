a80da75dc1ab66255027dc1f1f40fbdc5a4a22b0
cyberfly <info@cyberfly.io>
2024-12-28 16:54:03 +0530

    improvement

--- index.js
+++ index.js
@@ -9,13 +9,11 @@ import { peerIdFromString } from '@libp2p/peer-id'
 let bootstrap_nodes = ["/dns4/node.cyberfly.io/tcp/31001/p2p/12D3KooWA8mwP9wGUc65abVDMuYccaAMAkXhKUqpwKUZSN5McDrw"]
 
 const libp2p = await createLibp2p(getLibp2pOptions(bootstrap_nodes))
-
+let connected = 0
 libp2p.addEventListener('peer:connect', (evt) => {
     const peerId = evt.detail
     console.log('Connection established to:', peerId.toString())
   })
-
-
 
 const monitor_nodes = async()=>{
     const nodeData = await getNodeInfo()
@@ -31,21 +29,22 @@ const monitor_nodes = async()=>{
         const ma = multiaddr(element.multiaddr)
         const peerId = peerIdFromString(element.peer_id)
         libp2p.peerRouting.findPeer(peerId, {maxTimeout:1000}).then((peerInfo)=>{
-
-          console.log(peerId)
-
+        connected = connected + 1
+        console.log("found", element.peer_id)
+        console.log("connected", connected)
+        }, (error)=>{
           libp2p.dial(ma).then(d=>console.log(element.peer_id+ ` ${element.multiaddr} ` + ' - dial success')).catch(err=>{
           
-            if (!connectedPeers.includes(element.peer_id)){
-              console.log("disable", element.peer_id)
+            if (connectedPeers.includes(element.peer_id)){
+              console.log(element.peer_id+ " - connected to bootstrap node")
+              connected = connected + 1
             }
             else{
-              console.log(element.peer_id+ " - connected to bootstrap node")
+              console.log(element.peer_id, "not found")
+              disableNode(element.peer_id, element.multiaddr)
             }
+           
           })
-        }, (error)=>{
-          console.log(element.peer_id, "not found")
-          disableNode(element.peer_id, element.multiaddr)
         })
    
       });
@@ -53,3 +52,4 @@ const monitor_nodes = async()=>{
 }
 monitor_nodes()
 setInterval(monitor_nodes, 3600000)
+connected = 0
