1f68fe9f3879693f579471f3fe601c99104d37b1
cyberfly <info@cyberfly.io>
2025-01-23 17:44:57 +0530

    add disable node cap

--- pact-services.js
+++ pact-services.js
@@ -53,7 +53,8 @@ const getNodeInfo = async (peerId) =>{
     .addSigner(keyPair.publicKey, (withCapability)=>[
       withCapability('free.cyberfly-account-gas-station.GAS_PAYER', 'cyberfly-account-gas', { int: 1 }, 1.0),
       withCapability('free.cyberfly_node.ADMIN_GUARD'),
-      withCapability('free.cyberfly_node.BANK_DEBIT')
+      withCapability('free.cyberfly_node.BANK_DEBIT'),
+      withCapability('free.cyberfly_node.DISABLENODE', peerId)
     ])
     .setMeta({chainId:"1",senderAccount:"cyberfly-account-gas", gasLimit:2000, gasPrice:0.0000001})
     .setNetworkId("testnet04")
